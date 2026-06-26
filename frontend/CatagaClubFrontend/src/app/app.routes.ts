9289677d91880b615ee276d470eeabea58cdafd3
David-MT1 <dmendozat@unsa.edu.pe>
2026-06-26 10:05:29 -0500

    añadido de nosotros y editado de habitaciones

--- frontend/CatagaClubFrontend/src/app/app.routes.ts
+++ frontend/CatagaClubFrontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Habitaciones } from './pages/habitaciones/habitaciones';
 import { Sauna } from './pages/sauna/sauna';
+import { Nosotros } from './pages/nosotros/nosotros';
 
 export const routes: Routes = [
   {
@@ -16,5 +17,9 @@ export const routes: Routes = [
   {
     path: 'sauna',
     component: Sauna
+  },
+  {
+    path: 'nosotros',
+    component : Nosotros
   },
 ];
