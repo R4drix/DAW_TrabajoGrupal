a51c2f4dbdd3c7a4d2068662279ccdb9e04f1643
David-MT1 <dmendozat@unsa.edu.pe>
2026-07-10 10:39:34 -0500

    Guardando cambios antes de actualizar

---
+++ frontend/CatagaClubFrontend/src/app/shared/admin-footer/admin-footer.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-footer',
+  imports: [],
+  templateUrl: './admin-footer.html',
+  styleUrl: './admin-footer.css',
+})
+export class AdminFooter {}
